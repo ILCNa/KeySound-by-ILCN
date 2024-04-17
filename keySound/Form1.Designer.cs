0ca4dda2f4b4e9b6c73306b6f7f8da9eeacd7994
ILCNa <2433749091@qq.com>
2024-04-17 15:22:47 +0800

    a
    a

---
+++ keySound/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace keySound
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonTurn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.soundChoseBox = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.volumeSlider = new NAudio.Gui.VolumeSlider();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开机自启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonTurn
+            // 
+            this.buttonTurn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonTurn.Font = new System.Drawing.Font("霞鹜文楷", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonTurn.Location = new System.Drawing.Point(523, 0);
+            this.buttonTurn.Name = "buttonTurn";
+            this.buttonTurn.Size = new System.Drawing.Size(180, 214);
+            this.buttonTurn.TabIndex = 0;
+            this.buttonTurn.Text = "启动";
+            this.buttonTurn.UseVisualStyleBackColor = true;
+            this.buttonTurn.Click += new System.EventHandler(this.buttonTurn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(104, 216);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(91, 28);
+            this.菜单ToolStripMenuItem.Text = "控制面板";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.soundChoseBox});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(91, 28);
+            this.toolStripMenuItem1.Text = "参数设置";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 30);
+            this.toolStripTextBox1.Text = "选择音效包:";
+            // 
+            // soundChoseBox
+            // 
+            this.soundChoseBox.Name = "soundChoseBox";
+            this.soundChoseBox.Size = new System.Drawing.Size(121, 32);
+            this.soundChoseBox.TextChanged += new System.EventHandler(this.changePack);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(91, 28);
+            this.toolStripMenuItem2.Text = "关于程序";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.volumeSlider);
+            this.panel1.Controls.Add(this.buttonTurn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(104, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 216);
+            this.panel1.TabIndex = 2;
+            // 
+            // volumeSlider
+            // 
+            this.volumeSlider.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.volumeSlider.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.volumeSlider.Font = new System.Drawing.Font("霞鹜文楷", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.volumeSlider.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.volumeSlider.Location = new System.Drawing.Point(0, 0);
+            this.volumeSlider.Name = "volumeSlider";
+            this.volumeSlider.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.volumeSlider.Size = new System.Drawing.Size(523, 214);
+            this.volumeSlider.TabIndex = 1;
+            this.volumeSlider.VolumeChanged += new System.EventHandler(this.volumeChange);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "KeySound";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem,
+            this.toolStripMenuItem4,
+            this.开机自启动ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 163);
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.显示ToolStripMenuItem.Text = "显示";
+            this.显示ToolStripMenuItem.Click += new System.EventHandler(this.显示ToolStripMenuItem_Click);
+            // 
+            // 开机自启动ToolStripMenuItem
+            // 
+            this.开机自启动ToolStripMenuItem.Name = "开机自启动ToolStripMenuItem";
+            this.开机自启动ToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.开机自启动ToolStripMenuItem.Text = "开机自启动";
+            this.开机自启动ToolStripMenuItem.Click += new System.EventHandler(this.开机自启动ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(237, 6);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(240, 30);
+            this.toolStripMenuItem3.Text = "退出";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(240, 30);
+            this.toolStripMenuItem4.Text = "默认启动音效";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(809, 216);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("霞鹜文楷", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "KeySound";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTurn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private NAudio.Gui.VolumeSlider volumeSlider;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripComboBox soundChoseBox;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开机自启动ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+    }
+}
+
---
+++ keySound/Form1.cs
@@ -0,0 +1,376 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using IWshRuntimeLibrary;
+namespace keySound
+{
+    public partial class MainForm : Form
+    {
+        WaveOutEvent[] commonPlayers;
+        WaveOutEvent[] enterPlayers;
+        WaveOutEvent[] spacePlayers;
+        WaveOutEvent[] backSpacePlayers;
+
+        AudioFileReader[] commonAduioFiles;
+        AudioFileReader[] enterAduioFiles;
+        AudioFileReader[] spaceAduioFiles;
+        AudioFileReader[] backSpaceAduioFiles;
+        
+        string packName = "test";//加载音效包名
+
+        string[] packNames = Directory.GetFileSystemEntries(@"packs");
+
+
+
+        bool autoStart = false;
+        bool statring = false;//是否可以播放
+
+        string[] commonfiles;
+        string[] enterfiles;
+        string[] spacefiles;
+        string[] backSpacefiles;
+
+        Random random;
+        int randLast=0;//上一次随机结果
+        int randNow=0;//此次随机结果
+
+        Dictionary<Keys, bool> keyStates = new Dictionary<Keys, bool>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            initDirectory();
+            for (int i = 0; i < packNames.Length; i++) { packNames[i] = packNames[i].Split('\\')[1]; }
+            soundChoseBox.Text = packName;
+            soundChoseBox.Items.AddRange(packNames);
+
+            string[] args = Environment.GetCommandLineArgs();
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] == "-minimized")
+                {
+                    WindowState = FormWindowState.Minimized;
+                    ShowInTaskbar = false;
+                }
+
+            }
+            if (IsLnk()) 开机自启动ToolStripMenuItem.Checked = true;
+            else 开机自启动ToolStripMenuItem.Checked = false;
+
+            if (!System.IO.File.Exists("config.txt")){
+                using (StreamWriter writer = new StreamWriter("config.txt"))
+                {
+                    autoStart = false;
+                    writer.WriteLine("autoStart=false");
+                } }
+            using (StreamReader reader = new StreamReader("config.txt"))
+            {
+                string line = reader.ReadLine();
+                if (line.Split('=')[1] == "true") { autoStart = true;toolStripMenuItem4.Checked = true; statring = true; buttonTurn.Text = "停止"; }
+            }
+
+            // 注册全局键盘事件监听器
+            KeyboardHook.Start();
+            KeyboardHook.KeyDown += KeyboardHook_KeyDown;
+            KeyboardHook.KeyUp += KeyboardHook_KeyUp;
+
+        }
+        private bool IsLnk()
+        {
+
+            if (System.IO.File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), string.Format("{0}.lnk", "KeySound"))))
+                return true;
+            else return false;
+        }
+        private void initDirectory()
+        {
+            commonfiles = Directory.GetFiles(@"packs\" + packName + "\\common");
+            enterfiles = Directory.GetFiles(@"packs\" + packName + "\\enter");
+            spacefiles = Directory.GetFiles(@"packs\" + packName + "\\space");
+            backSpacefiles = Directory.GetFiles(@"packs\" + packName + "\\backspace");
+
+            commonPlayers = new WaveOutEvent[commonfiles.Length];
+            enterPlayers = new WaveOutEvent[enterfiles.Length];
+            spacePlayers = new WaveOutEvent[spacefiles.Length];
+            backSpacePlayers = new WaveOutEvent[backSpacefiles.Length];
+
+            commonAduioFiles = new AudioFileReader[commonfiles.Length];
+            enterAduioFiles = new AudioFileReader[enterfiles.Length];
+            spaceAduioFiles = new AudioFileReader[spacefiles.Length];
+            backSpaceAduioFiles = new AudioFileReader[backSpacefiles.Length];
+
+            initWaveOutEvents(commonfiles, commonPlayers, commonAduioFiles);
+            initWaveOutEvents(enterfiles, enterPlayers, enterAduioFiles);
+            initWaveOutEvents(spacefiles, spacePlayers, spaceAduioFiles);
+            initWaveOutEvents(backSpacefiles, backSpacePlayers, backSpaceAduioFiles);
+
+            random = new Random(GetHashCode());
+        }
+
+        private void initWaveOutEvents(String[] commonfiles,WaveOutEvent[] commonPlayers,AudioFileReader[] commonAduioFiles)
+        {
+            for (int i = 0; i < commonfiles.Length; i++)
+            {
+                commonAduioFiles[i] = new AudioFileReader(commonfiles[i]);
+                commonPlayers[i] = new WaveOutEvent();
+                commonPlayers[i].Init(commonAduioFiles[i]);
+            }
+        }
+
+        private void KeyboardHook_KeyUp(Keys key)
+        {
+            keyStates[key] = false;
+        }
+
+        private void KeyboardHook_KeyDown(Keys key)
+        {
+            if (!keyStates.ContainsKey(key)|| keyStates[key] == false)
+            {
+                
+                keyStates[key] = true;
+                if (key.Equals(Keys.Enter))
+                {
+                    playSound(enterPlayers,enterAduioFiles);
+                }
+                else if (key.Equals(Keys.Space))
+                {
+                    playSound(spacePlayers,spaceAduioFiles);
+                }
+                else if (key.Equals(Keys.Back))
+                {
+                    playSound(backSpacePlayers,backSpaceAduioFiles);
+                }
+                else
+                {
+                    playSound(commonPlayers,commonAduioFiles);
+                }
+              
+            }
+     
+            else return;
+
+        }
+
+        private void buttonTurn_Click(object sender, EventArgs e)
+        {
+            if (statring == false) { statring = true; buttonTurn.Text = "停止";
+
+            }
+            else
+            {
+                statring = false ; buttonTurn.Text = "启动";
+            }
+        }
+
+
+        void playSound(WaveOutEvent[] commonPlayers, AudioFileReader[] commonAduioFiles)
+        {
+            if(statring == false) {return; }
+
+  
+
+            randLast =randNow=random.Next()%commonAduioFiles.Length;
+            if (commonAduioFiles[randNow].Position != 0) commonAduioFiles[randNow].Position = 0;
+            commonPlayers[randNow].Play();
+
+            
+        }
+
+
+        private void volumeChange(object sender, EventArgs e)
+        {
+            for (int i = 0;i<commonfiles.Length;i++)
+                commonPlayers[i].Volume = volumeSlider.Volume;
+        }
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+
+            // 在窗体关闭时取消全局键盘事件监听器
+            KeyboardHook.KeyDown -= KeyboardHook_KeyDown;
+            KeyboardHook.Stop();
+        }
+
+        private void changePack(object sender, EventArgs e)
+        {
+            packName=soundChoseBox.Text;
+            initDirectory();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("欢迎使用KeySound\n反馈请联系2433749091@qq.com","关于");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)    //最小化到系统托盘
+            {
+                e.Cancel = true;
+                WindowState = FormWindowState.Minimized;
+
+                ShowInTaskbar = false;
+
+            }
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1_DoubleClick(sender);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+
+            ShowInTaskbar = true;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender)
+        {
+            WindowState = FormWindowState.Normal;
+
+            ShowInTaskbar = true;
+        }
+
+        private void 开机自启动ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!开机自启动ToolStripMenuItem.Checked)
+            {
+                开机自启动ToolStripMenuItem.Checked = true;
+
+                creatLnk();
+
+            }
+            else
+            {
+                开机自启动ToolStripMenuItem.Checked = false;
+                deleteLnk();
+            }
+        }
+
+        private void deleteLnk()
+        {
+            System.IO.File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), string.Format("{0}.lnk", "KeySound")));
+        }
+
+        private void creatLnk()
+        {
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), string.Format("{0}.lnk", "KeySound")));//创建快捷方式对象
+            shortcut.TargetPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "KeySound.exe"; //标路径
+            shortcut.Arguments = "-minimized";
+            shortcut.WorkingDirectory = Path.GetDirectoryName(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "KeySound.exe");//设置起始位置
+            shortcut.WindowStyle = 7;//设置运行方式，默认为常规窗口
+            shortcut.Description = "Nothing";//设置备注
+            shortcut.IconLocation = string.IsNullOrWhiteSpace(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "resources/main.ico") ? AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "KeySound.exe" : AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"resources/main.ico";//设置图标路径
+            shortcut.Save();//保存快捷方式
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //自动启动音效
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+           
+            if(toolStripMenuItem4.Checked)
+            {
+                using (StreamWriter writer = new StreamWriter("config.txt"))
+                {
+                    toolStripMenuItem4.Checked = false;
+                    autoStart = false;
+                    writer.WriteLine("autoStart=false");
+                }
+            }
+            else
+            using (StreamWriter writer = new StreamWriter("config.txt"))
+            {
+                    toolStripMenuItem4.Checked = true;
+                    autoStart = true;
+                    writer.WriteLine("autoStart=true");
+            }
+        }
+    }
+
+    public static class KeyboardHook
+    {
+        public delegate void KeyDownHandler(Keys key);
+        public static event KeyDownHandler KeyDown;
+
+        public delegate void KeyUpHandler(Keys key);
+        public static event KeyUpHandler KeyUp; // 添加键盘抬起事件
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private static LowLevelKeyboardProc proc = HookCallback;
+        private static IntPtr hookId = IntPtr.Zero;
+
+        public static void Start()
+        {
+            hookId = SetHook(proc);
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(hookId);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                KeyDown?.Invoke((Keys)vkCode);
+            }
+            else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP) // 捕获键盘抬起事件
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                KeyUp?.Invoke((Keys)vkCode);
+            }
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
